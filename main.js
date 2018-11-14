c4b75a1eed33774477f9d1b65dfa98a77cd5d125
Roger Rudin <roger.rudin@lambda-it.ch>
2018-11-14 11:13:03 +0100

    inital commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const url = require('url');
+const path = require('path');
+let win;
+
+ipcMain.on('start', (sender, data) => {
+    console.log('electron Version wurde geclickt!');
+    win.webContents.send('finish', true);
+});
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.on('closed', () => {
+        // dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    // on macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // on macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if(win === null) {
+        createWindow();
+    }
+});
+
